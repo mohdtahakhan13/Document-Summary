34c8a4551e605e5b2ba374836fd87c0eacd436ee
mohdtahakhan13 <mohdtahakhan13@gmail.com>
2025-09-26 17:14:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,56 +1,3 @@
-// import express from "express";
-// const app = express();
-
-// import dotenv from "dotenv";
-// dotenv.config();
-// import signupRoutes from "./routes/signup.js";
-// import logoutRoutes from "./routes/logout.js";
-// import loginRoutes from "./routes/login.js";
-// import authRouter from "./routes/authRouter.js";
-// import dashboard from "./routes/dashboard.js";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import jwt from "jsonwebtoken";
-// import path from "path";
-
-// const __dirname = path.resolve();
-
-// app.use(
-//   cors({
-//     origin: process.env.ORIGIN,
-//     credentials: true,
-//   })
-// );
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use("/api/signup", signupRoutes);
-// app.use("/api/logout", logoutRoutes);
-// app.use("/api/login", loginRoutes);
-// app.use("/api/auth", authRouter);
-// app.use("/api/dashboard", dashboard);
-
-// app.use(express.static(path.join(__dirname, "/client/dist")));
-// app.get("/api/check-auth", (req, res) => {
-//   const token = req.cookies.token;
-//   if (!token) {
-//     return res.json({ loggedIn: false });
-//   }
-
-//   try {
-//     const email = jwt.verify(token, process.env.JWT_SECRET);
-//     res.json({ loggedIn: true });
-//   } catch (err) {
-//     res.json({ loggedIn: false });
-//   }
-// });
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-// });
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 import express from "express";
 const app = express();
 
@@ -71,7 +18,8 @@ app.use(
   cors({
     origin: [
       process.env.ORIGIN, 
-      "https://document-summary-one.vercel.app"
+      "https://document-summary-one.vercel.app",
+      "http://localhost:5173/",
     ], 
     credentials: true,
   })
